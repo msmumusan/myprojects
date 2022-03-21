d554a896abc5eef7491cdbf3542df91857202bd8
MuMuSan <[mumusan963@gmail.com]>
2022-03-21 10:12:07 +0630

    first commit

---
+++ custom-authentication-provider-demo/src/main/java/com/example/customauthenticationproviderdemo/CustomAuthenticationProviderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.customauthenticationproviderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthenticationProviderDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomAuthenticationProviderDemoApplication.class, args);
+    }
+
+}
---
+++ custom-authentication-provider-demo/src/test/java/com/example/customauthenticationproviderdemo/CustomAuthenticationProviderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customauthenticationproviderdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAuthenticationProviderDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
